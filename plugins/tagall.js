8801050d74b025f94546c34712ed26d23b5a067e
puttusdas <kenichidas013@gmail.com>
2026-09-10 19:20:14 +0530

    PUTTUS-BOT

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -10,6 +10,31 @@ module.exports = {
   async handler(sock, message, args, context) {
     const { chatId, channelInfo } = context;
 
+    // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    // VCard
+    // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    const vcard =
+      'BEGIN:VCARD\n' +
+      'VERSION:3.0\n' +
+      'FN:🌸•𝐏𝐮𝐭ᴛᴜꜱ•⌲\n' +
+      'ORG:PUTTUS BOT;\n' +
+      'TEL;type=CELL;type=VOICE;waid=918967360566:+91 8967360566\n' +
+      'END:VCARD';
+
+    const vcardReply = {
+      key: {
+        fromMe: false,
+        participant: '918967360566@s.whatsapp.net',
+        remoteJid: 'status@broadcast'
+      },
+      message: {
+        contactMessage: {
+          displayName: '⎯꯭̽ꪹ𝐏ᴜᴛᴛᴜs-𝐁ᴏᴛ⎯꯭̽💜',
+          vcard
+        }
+      }
+    };
+
     try {
       const groupMetadata = await sock.groupMetadata(chatId);
       const participants = groupMetadata.participants;
@@ -21,30 +46,38 @@ module.exports = {
             text: "No participants found in the group.",
             ...channelInfo,
           },
-          { quoted: message },
+          { quoted: vcardReply }
         );
         return;
       }
 
-      let messageText = "🔊𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐔𝐓𝐓𝐔𝐒-𝐃𝐀𝐒 😎 𝐃𝐔𝐁𝐀𝐈-𝐃𝐎𝐍:\n\n";
+      let messageText = "━[ 𝐏ᴜᴛᴛᴜꜱ - 𝐃ᴀꜱ ]━:\n\n";
+
       participants.forEach((participant) => {
-        messageText += `@${participant.id.split("@")[0]}\n`;
+        const number = participant.id.split("@")[0];
+        messageText += `@${number}\n`;
       });
 
-      await sock.sendMessage(chatId, {
-        text: messageText,
-        mentions: participants.map((p) => p.id),
-        ...channelInfo,
-      });
+      await sock.sendMessage(
+        chatId,
+        {
+          text: messageText,
+          mentions: participants.map((p) => p.id),
+          ...channelInfo,
+        },
+        { quoted: vcardReply }
+      );
+
     } catch (error) {
       console.error("Error in tagall command:", error);
+
       await sock.sendMessage(
         chatId,
         {
           text: "Failed to tag all members.",
           ...channelInfo,
         },
-        { quoted: message },
+        { quoted: vcardReply }
       );
     }
   },
