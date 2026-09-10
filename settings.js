d7a599fa43d045b329006a9ed96eae4f3fd5ec9e
puttusdas <kenichidas013@gmail.com>
2026-09-10 10:37:59 +0530

    PUTTUS-AI

--- settings.js
+++ settings.js
@@ -1,23 +1,17 @@
 const settings = {
-  prefixes: [".", "!", "/", "#"], // Multiple prefix support you can add one or more
-  packname: "PUTTUS-AI",
-  author: "‎puttus-das",
+  prefixes: [process.env.BOT_PREFIX || "."],
+  botName: process.env.BOT_NAME || "PUTTUS-XD",
+  menuImageUrl: process.env.MENU_IMAGE_URL || "",
+  botOwner: process.env.BOT_OWNER || "Puttus Das",
+  ownerNumber: process.env.OWNER_NUMBER || "",
   timeZone: "Asia/Kolkata",
-  botName: "PUTTUS-AI", // your bot name
-  botOwner: "Puttus Das", // set your name
-  ownerNumber: "918967360566", // Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: "qnl7ssQChTdPjsKta2Ax2LMaGXz303tq",
   commandMode: "public",
   maxStoreMessages: 20,
-  tempCleanupInterval: 1 * 60 * 60 * 1000, // 1 hours
   storeWriteInterval: 10000,
-  description:
-    "This is a bot for managing group commands and automating tasks.",
-  version: "5.1.0",
-  updateZipUrl:
-    "https://github.com/puttus-das/PUTTUS-AI/archive/refs/heads/main.zip",
-  channelLink: "https://whatsapp.com/channel/0029Vb7pmbEEwEjzdGSM4G3B",
-  ytch: "puttus-das",
+  version: "6.0.0",
+  channelLink:
+    process.env.CHANNEL_LINK ||
+    "https://whatsapp.com/channel/0029Vb7pmbEEwEjzdGSM4G3B",
 };
 
 module.exports = settings;
